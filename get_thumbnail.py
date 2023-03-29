61bd0028474da77ba1865447d244c08e0716feb3
kosmolub01 <szczdwo746@student.polsl.pl>
2023-03-29 19:33:03 +0200

    Change url parsing

--- get_thumbnail.py
+++ get_thumbnail.py
@@ -12,12 +12,27 @@ Input parameters:
 import sys
 import requests
 import shutil
+import cv2
+import numpy as np
+
+# To compare two images
+def mse(img1, img2):
+   h, w = img1.shape
+   diff = cv2.subtract(img1, img2)
+   err = np.sum(diff**2)
+   mse = err/(float(h*w))
+   return mse
 
 if __name__ == "__main__":
 
-    url = sys.argv[1]
-    response = requests.get(url, stream=True)
+    # Create thumbnail url
+    video_url = sys.argv[1]
+    video_id = video_url[video_url.index("=") + 1: ]
+    thumbnail_url = "https://img.youtube.com/vi/{}/0.jpg"
+    thumbnail_url = thumbnail_url.format(video_id)
+
+    # Request and save image in given location
+    response = requests.get(thumbnail_url, stream=True)
     with open(sys.argv[2], "wb") as out_file:
         shutil.copyfileobj(response.raw, out_file)
     del response
-
