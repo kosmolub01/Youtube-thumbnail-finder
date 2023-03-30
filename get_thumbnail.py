2d18942804dd90cf58d6df6651eba8ce8e66e473
kosmolub01 <szczdwo746@student.polsl.pl>
2023-03-30 12:31:03 +0200

    Add downloading video functionality

--- get_thumbnail.py
+++ get_thumbnail.py
@@ -2,37 +2,50 @@
 ================================================================================
 Filename: get_thumbnail.py
 Description:
-Script finds the thumbnail of a given YouTube's video and saves it in a given 
-location.
+Script finds the thumbnail of a given YouTube's video and saves it in current 
+working directory. Video is also downloaded.
 Input parameters:
     - YouTube's video url 
-    - path to location to save the thumbnail
 ================================================================================
+
+TODO:
+- input validation
+- exception handling (no suitable stream for video)
+- remove saving thumbnail functionality
+
 """
 import sys
+import os
 import requests
 import shutil
 import cv2
 import numpy as np
-
-# To compare two images
-def mse(img1, img2):
-   h, w = img1.shape
-   diff = cv2.subtract(img1, img2)
-   err = np.sum(diff**2)
-   mse = err/(float(h*w))
-   return mse
+from pytube import YouTube
 
 if __name__ == "__main__":
 
-    # Create thumbnail url
+    # Save program parameter
     video_url = sys.argv[1]
-    video_id = video_url[video_url.index("=") + 1: ]
-    thumbnail_url = "https://img.youtube.com/vi/{}/0.jpg"
-    thumbnail_url = thumbnail_url.format(video_id)
 
-    # Request and save image in given location
+    # YouTube object with URL of desired video
+    yt = YouTube(video_url)
+
+    # Get filtered stream for the video
+    stream = yt.streams.filter(adaptive = True, mime_type="video/mp4").first()
+
+    # Download video
+    stream.download()
+
+    # Request thumbnail image
+
+    thumbnail_url = yt.thumbnail_url
+
     response = requests.get(thumbnail_url, stream=True)
-    with open(sys.argv[2], "wb") as out_file:
+    with open(os.getcwd() + "\\thumbnail.png", "wb") as out_file:
         shutil.copyfileobj(response.raw, out_file)
     del response
+
+
+    
+
+
