06dc78a987a6adf8cd66d6d116458dee8afe5ce3
kosmolub01 <szczdwo746@student.polsl.pl>
2023-04-17 16:38:12 +0200

    Handle command from view

--- controller.py
+++ controller.py
@@ -1,14 +1,34 @@
 import sys
 from model import Model
+from view import View
 
 class Controller:
+    """
+    Controller class of "Youtube thumbnail finder" app.
+
+    TBD:
+    Attributes
+    ----------
+    name : str
+        first name of the person
+
+    Methods
+    -------
+    info(additional=""):
+        Prints the person's name and age.
+    """
+
     def __init__(self) -> None:
-        # Save program parameter
-        video_url = sys.argv[1]
-        self.model = Model(video_url)
+        self.model = Model()
+        self.view = View(self)
 
     def main(self):
-        self.model.process_video()
+        self.view.main()
+
+    def button_on_click(self):       
+        self.model.input_url = self.view.input_url.get()       
+        output_url = self.model.process_video()
+        self.view.output_url.set(output_url)
 
 if __name__ == "__main__":
 
